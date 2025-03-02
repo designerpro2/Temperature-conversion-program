d6735ac90304de95c82cb709cf460828a747d1ac
HomeLander <109650236+designerpro2@users.noreply.github.com>
2025-03-02 12:09:24 +0500

    Add files via upload

---
+++ Temperature Coversion Program/index.js
@@ -0,0 +1,28 @@
+//assigning the values and variables
+
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+
+let temp;
+
+
+
+
+function convert(){
+    if(toFahrenheit){
+        temp = Number(textBox.value);
+        temp = (temp * 9/5) + 32;
+        result.textContent = temp.toFixed(1) + "°F";
+        
+    }
+    else if(toCelsius){
+        temp = Number(textBox.value);
+        temp = (temp - 32) * 5/9;
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else{
+        result.textContent = "please select a conversion"
+    }
+}
